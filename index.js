470d6b42c96588acda24cb2f1c02274b3f3240a9
paulinomreyes <paulinomreyes@gmail.com>
2017-10-19 00:34:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-        res.end("Welcome to Node.js App version 2");
+        res.end("Welcome to Node.js App version 2.5");
 });
 
 server.listen(8080);
